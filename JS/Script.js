f9ad75529cd2f005c18f1fd02b25dc30d1426a72
Jefferson Barbosa <119863688+Jefferson-BR@users.noreply.github.com>
2023-04-10 14:30:01 -0300

    Add files via upload
    Code Website

---
+++ JS/Script.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.title-animate', { 
+    rotate: {x: 80, y: 0, z: 0},
+    duration: 3000,
+  })
+  sr.reveal('.Titulo-Input', { 
+    rotate: {x: 80, y: 0, z: 0},
+    duration: 3000,
+  }) 
+  sr.reveal('.Senha-Input', { 
+    rotate: {x: 80, y: 0, z: 0},
+    duration: 3000,
+  })
+  sr.reveal('.login', { 
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 3000,
+  })
+  sr.reveal('.Senha', { 
+    rotate: {x: 80, y: 0, z: 0},
+    duration: 3000,
+  }) 
